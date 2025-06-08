34af069d7c1525f9e4d29e66f3707db5bc282dfe
Thiago Zeni Bazotti <thiagozb@gmail.com>
2025-06-08 11:03:28 -0300

    feat: implement session creation

---
+++ pages/api/v1/sessions/index.js
@@ -0,0 +1,23 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import authentication from "models/authentication.js";
+import session from "models/session.js";
+
+const router = createRouter();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password,
+  );
+
+  const newSession = await session.create(authenticatedUser.id);
+
+  return response.status(201).json(newSession);
+}
